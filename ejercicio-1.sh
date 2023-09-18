49ecb6aa061ac7a96fe5730e93c396a4284ec25a
Ricardo <antequerarj@gmail.com>
2023-09-18 19:29:23 -0300

    primer commit

---
+++ ejercicio-1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nombre="Ricardo"
+apellido="Antequera"
+
+echo "Te llamas: "$nombre $apellido
---
+++ ejercicio-4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "descarga"
+
+sudo apt install python3
+
+echo "fin de la descarga"
